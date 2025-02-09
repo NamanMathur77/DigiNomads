071064c1427171a188c531cd42c0e50ab5ddff73
NamanMathur77 <namanmathur619@gmail.com>
2025-02-09 19:40:14 +0530

    modifying city api to get use limit and offset

--- Server/main.py
+++ Server/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends
+from fastapi import Depends, Query
+from sqlalchemy.orm import Session
 from typing import List
 import config
 from coordinate import Coordinate
@@ -7,9 +8,12 @@ from city import City
 app = config.create_app()
 
 @app.get("/cities")
-def get_cities(db: Session = Depends(config.Config.get_db)):
+def get_cities(
+    db: Session = Depends(config.Config.get_db),
+    limit: int = Query(10, description="Number of cities to fetch"),
+    offset: int = Query(0, description="Number of cities to skip")):
     cities_list : List[City]
-    cities_list = db.query(City).limit(10).all()
+    cities_list = db.query(City).offset(offset).limit(limit).all()
     for city in cities_list:
         city.coordinates = Coordinate(city.lat, city.lng)
     return cities_list
