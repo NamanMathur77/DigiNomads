77c2fc02beacf8cb6b1fac7ad33de98100363a15
NamanMathur77 <namanmathur619@gmail.com>
2025-02-10 21:50:05 +0530

    adding the endpoint for closest city to the given coordinates

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,7 @@
+import json
 from fastapi import Depends, Query
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from typing import List
 import config
 from coordinate import Coordinate
@@ -17,3 +19,28 @@ def get_cities(
     for city in cities_list:
         city.coordinates = Coordinate(city.lat, city.lng)
     return cities_list
+
+@app.get("/closest-city")
+def get_closest_cities(
+    db: Session = Depends(config.Config.get_db),
+    limit: int = Query(2, description="Number of closest cities to the given coordinate"),
+    lat: float = Query(..., description="Latitude of the location"),
+    lng: float = Query(..., description="longitude of the location")
+):
+    query= text("""
+    SELECT id, city_ascii, lat, lng,
+        (6371 * ACOS(COS(RADIANS(:lat)) * COS(RADIANS(lat)) * 
+        COS(RADIANS(lng) - RADIANS(:lng)) + 
+        SIN(RADIANS(:lat)) * SIN(RADIANS(lat)))) AS distance
+        FROM cities
+        ORDER BY distance ASC
+        LIMIT :limit
+    """)
+    result = db.execute(query, {"lat": lat, "lng": lng, "limit": limit}).all()
+    results = [tuple(row) for row in result]
+    print(result)
+    breakpoint
+    if not result:
+        return {"message": "No cities found"}
+
+    return json.dumps(results)
